9bf1ea1b50b9f67d480871ccad72dbe834cba70e
RcN1912 <raccoon1912@gmail.com>
2021-03-07 10:55:19 -0300

    Remove a feature

--- ButtonMenu.js
+++ ButtonMenu.js
@@ -1,15 +1,12 @@
 const menu = document.querySelector('.Menu')
 const ButtonMenu_Open = document.querySelector('.ButtonMenu_Open')
 const ButtonMenu_close = document.querySelector('.ButtonMenu_close')
-const MenuItem = document.querySelector('.MenuItem')
+
 
 ButtonMenu_Open.addEventListener('click', function(){
     menu.classList.add('Menu_Open')
 })
 
 ButtonMenu_close.addEventListener('click', function(){
-    menu.classList.remove('Menu_Open')
-})
-MenuItem.addEventListener('click',function(){
     menu.classList.remove('Menu_Open')
 })
