dd9648cf64c3bfd06c8ce9de6d0f178c21ba6f44
RcN1912 <raccoon1912@gmail.com>
2021-03-07 12:24:31 -0300

    New feature menu animation

--- ButtonMenu.js
+++ ButtonMenu.js
@@ -1,7 +1,6 @@
 const menu = document.querySelector('.Menu')
 const ButtonMenu_Open = document.querySelector('.ButtonMenu_Open')
 const ButtonMenu_close = document.querySelector('.ButtonMenu_close')
-const links = document.querySelectorAll('.Menu li');
 
 ButtonMenu_Open.addEventListener('click', function(){
     menu.classList.add('Menu_Open')
@@ -11,8 +10,4 @@ ButtonMenu_close.addEventListener('click', function(){
     menu.classList.remove('Menu_Open')
 })
 
-links.forEach(link =>{ //para cada li adiciona um eventListener click
-    link.addEventListener('click',function(){
-        menu.classList.remove('Menu_Open')
-    })
-})
+
--- menuAnimation.js
+++ menuAnimation.js
@@ -1,8 +1,14 @@
 const menuItems = document.querySelectorAll('ul li a[href^="#');
+const links = document.querySelectorAll('.Menu li');
 
 menuItems.forEach (item =>{ //para cada item no menu adiciona um eventListener
     item.addEventListener('click', scrollToIdOnClick)
 });
+links.forEach(link =>{ //para cada li adiciona um eventListener click
+    link.addEventListener('click',function(){
+        menu.classList.remove('Menu_Open')
+    })
+})
 
 function scrollToIdOnClick(event) {
     const element = event.target;
